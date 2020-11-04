c55d7b5466aad3e13403b4ca2106e69223764ecd
[Gustavo Picinato] <[gustavopicinato11@gmail.com]>
2020-11-03 23:17:15 -0300

    Adicionei componentes da home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/cabecalho/header';
 import Footer from './components/rodape/Footer';
+import Home from './components/homepage/Home';
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
       <Footer/>
     </div>
   );
